d53c5d18becbbdeb16225989ead1245fb376569e
Nana Sakisaka <saki7@setna.io>
2017-10-29 08:08:32 +0900

    JavaScript's module system really sucks

--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'js/[name].js',
-      // library: 'Nagato',
+      library: 'Nagato',
     },
     module: {
       rules: [
